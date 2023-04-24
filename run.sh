2d34daf6f9f7e5eaf3b11c7f7cfdc29e6b6b77dd
Matt Goldman <matt@github.mattgoldman.us>
2023-04-24 11:28:26 -0400

    run updates

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-# gunicorn --reload --bind 0.0.0.0:8000 --workers 4 hmnmp.wsgi:application
+ 
 python manage.py runserver --noreload --insecure
+# gunicorn --reload --bind 0.0.0.0:8000 --workers 4 hmnmp.wsgi:application
